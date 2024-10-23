a8819123a4de37d7fd7f3c41a7eb9e195a3c5bec
Guilherme Lopreti <guilherme.llopreti@gmail.com>
2024-10-23 00:23:59 -0300

    feat: user model schema

---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const User = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+mongoose.model('user', User)
