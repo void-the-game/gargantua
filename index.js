01df9ceb01ebbf9aacaec5bcc9f198714dc1caed
Guilherme Lopreti <guilherme.llopreti@gmail.com>
2024-10-23 00:17:22 -0300

    fix: db connection initialization

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import app from './app.js'
+import dbConnection from './config/connection.js'
 
 const PORT = process.env.PORT || 3000
+
+// Chamar a conexão com o banco de dados quando o servidor iniciar
+dbConnection()
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
