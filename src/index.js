2951cae5cbc76511b9df6e148d6f713fd0e1213d
Luiz Fernando <luiz.fpimentel0@gmail.com>
2024-10-28 01:09:41 -0300

    refactor: set default app port

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import app from './app.js'
 import dbConnection from '../config/connection.js'
 
-const PORT = process.env.PORT || 3000
-
 // Chamar a conexão com o banco de dados quando o servidor iniciar
 dbConnection()
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
+app.listen(3000, () => {
+  console.log(`listening on port 3000`)
 })
