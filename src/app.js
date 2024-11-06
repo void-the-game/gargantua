9184a0e0f322d26dd25bb37f41dc35c6e434c2cd
Guilherme Lopreti <guilherme.llopreti@gmail.com>
2024-11-06 12:15:59 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import router from './routes/index.js'
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.use(cors())
 app.use('/api', router)
 
 export default app
