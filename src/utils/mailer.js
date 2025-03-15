4fb4c770c38938132030ff5382c9db6b73de5ec9
Luiz Fernando <luiz.fpimentel0@gmail.com>
2025-03-15 12:21:54 -0300

    chore(mailer): remove  oauth

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,22 +1,7 @@
 import dotenv from 'dotenv'
 import nodemailer from 'nodemailer'
-import { google } from 'googleapis'
 
 dotenv.config()
-
-const OAuth2 = google.auth.OAuth2
-
-const oAuth2Client = new OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  process.env.CLIENT_URI
-)
-
-oAuth2Client.setCredentials({
-  refresh_token: process.env.REFRESH_TOKEN,
-})
-
-const accessToken = await oAuth2Client.getAccessToken()
 
 export const sendEmail = async (email, subject, text) => {
   try {
@@ -24,19 +9,14 @@ export const sendEmail = async (email, subject, text) => {
       host: process.env.EMAIL_HOST,
       service: process.env.EMAIL_SERVICE,
       port: 465,
-      secure: true,
+      secure: false,
       auth: {
-        type: 'OAuth2',
         user: process.env.EMAIL,
         pass: process.env.PASSWORD,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-        accessToken,
       },
       tls: {
-        rejectUnauthorized: false,
-      }
+        rejectUnauthorized: true,
+      },
     })
 
     await transporter.sendMail({
