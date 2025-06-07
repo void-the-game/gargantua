54dd1ee46c0a7b09c029f2adbb41948525ec3482
Luiz Fernando <luiz.fpimentel0@gmail.com>
2025-06-07 01:17:13 -0300

    feat: add Token and User repository interfaces

---
+++ src/domain/repositories/UserRepository.ts
@@ -0,0 +1,11 @@
+import { User } from '@/domain/entities/User'
+
+export interface UserRepository {
+  getUser(username: string): Promise<User | null>
+  getUserByEmail(email: string): Promise<User | null>
+  create(user: User): Promise<User>
+  userExistsByEmail(email: string): Promise<unknown>
+  userExistsByUsername(username: string): Promise<unknown>
+  update(userId: string, data: Partial<User>): Promise<void>
+  comparePassword(userId: string, plainPassword: string): Promise<boolean>
+}
