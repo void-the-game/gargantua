2886de3dddee5a4e949b3780850e39433343d324
Luiz Fernando <luiz.fpimentel0@gmail.com>
2025-06-08 03:15:51 -0300

    feat: implement VerifyEmailUseCase for email verification process

---
+++ src/application/useCases/VerifyUserEmailUseCase.ts
@@ -0,0 +1,37 @@
+import { TokenRepository } from '@/domain/repositories/TokenRepository'
+import { UserRepository } from '@/domain/repositories/UserRepository'
+import { TokenService } from '@/domain/services/TokenService'
+
+type VerfiyEmailUseCaseResponse = {
+  success: boolean
+  message?: string
+}
+
+enum ERRORS_MESSAGES {
+  INVALID_TOKEN = 'Invalid token',
+  TOKEN_EXPIRED = 'Token is either expired or not found',
+}
+
+export class VerifyEmailUseCase {
+  constructor(
+    private tokenService: TokenService,
+    private tokenRepository: TokenRepository,
+    private userRepository: UserRepository
+  ) { }
+
+  async execute(token: string): Promise<VerfiyEmailUseCaseResponse> {
+    const { userId, isValid } = await this.tokenService.verifyToken(token)
+    if (!isValid) return { success: false, message: ERRORS_MESSAGES.INVALID_TOKEN }
+
+    const storedToken = await this.tokenRepository.find(token, userId)
+
+    if (!storedToken || storedToken.expiresAt < new Date()) {
+      return { success: false, message: ERRORS_MESSAGES.TOKEN_EXPIRED }
+    }
+
+    await this.userRepository.update(userId, { verified: true })
+
+    const deletedToken = await this.tokenRepository.delete(storedToken.token)
+    return { success: deletedToken }
+  }
+}
